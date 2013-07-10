753b748ddbcbd0e5b9b23c5dfa8c55d628ca9bd2
Dan Sanford <dano@sanford9.com>
2013-07-10 15:09:21 +0000

    changed source of hello world

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+buf = fs.readFileSync('index.html', 'ascii');
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buf.toString('ascii'));
 });
 
 var port = process.env.PORT || 5000;
